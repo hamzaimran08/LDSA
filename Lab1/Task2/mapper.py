eb8657bcd3f63094d99039550bfca99f8d8a83c3
Hamza <hamzaimran08@gmail.com>
2019-04-04 20:19:44 +0200

    task2 json test

--- Lab1/Task2/mapper.py
+++ Lab1/Task2/mapper.py
@@ -2,18 +2,21 @@
 """mapper.py"""
 
 import sys
+import json
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
     # split the line into words
-    words = line.split()
+#     words = line.split()
+    j = json.loads(line)
+    print(j['id'])
     # increase counters
-    for word in words:
+#     for word in words:
         # write the results to STDOUT (standard output);
         # what we output here will be the input for the
         # Reduce step, i.e. the input for reducer.py
         #
         # tab-delimited; the trivial word count is 1
-        print('%s\t%s' % (word, 1))
+        # print('%s\t%s' % (word, 1))
