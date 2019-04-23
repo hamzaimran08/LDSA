e8af519be72654daee1a7f6cde3fb3476937d378
hamzaimran08 <hamzaimran08@gmail.com>
2019-04-23 21:55:08 +0000

    final changes task 2

--- Lab1/Task2/mapper.py
+++ Lab1/Task2/mapper.py
@@ -3,15 +3,29 @@
 
 import sys
 import json
+import re
 
 # input comes from STDIN (standard input)
 for line in sys.stdin:
     # remove leading and trailing whitespace
     line = line.strip()
+    if len(line) == 0:
+     continue
+    pronouns = ["han", "hon", "den", "det", "denna", "denne","hen"]
     # split the line into words
 #     words = line.split()
-    j = json.loads(line)
-    print(j['id'])
+    myJson = json.loads(line)
+    print('%s\t%s' % ("TOTAL", 1))
+    if 'retweeted_status' not in myJson:
+     print('%s\t%s' % ("UNIQUE", 1))
+     #print(myJson['retweeted_status'])
+     text = myJson['text'].lower()
+     textArray = text.split(" ")
+     #print(*textArray)
+     for word in pronouns:
+      if textArray.count(word) > 0:
+       print('%s\t%s' % (word, 1))
+#       print(x)
     # increase counters
 #     for word in words:
         # write the results to STDOUT (standard output);
